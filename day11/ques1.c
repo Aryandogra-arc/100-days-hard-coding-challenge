05df61b0aad57de634a56ff88c21fd9366f6456f
Aryandogra-arc <Aryan.40678@stu.upes.ac.in>
2026-08-22 22:17:29 +0530

    Add files via upload

---
+++ day11/ques1.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+int main() {
+int a;
+    printf("enter the number:");
+    scanf("%d", &a);
+    if(a == 1)
+{
+    printf("janurary, 31 days");
+}
+else if (a == 2)
+{
+printf("feburary, 28 days ");
+}
+else if(a == 3)
+{
+printf("march, 31 days ");
+}
+else if(a == 4)
+{
+printf("april, 30 days");
+}
+else if(a == 5)
+{
+    printf("may, 31 days");
+}
+else if(a == 6)
+{
+    printf("june, 30 days");
+}
+else if(a == 7)
+{
+    printf("july, 31 days");
+}
+else if(a == 8)
+{
+    printf("august, 31 days");
+}
+else if(a == 9)
+{
+    printf("setember, 30 days");
+}
+else if(a ==10)
+{
+    printf("october, 31 days");
+}
+else if(a == 11)
+{
+    printf("november, 30 days");
+}
+else if(a == 12)
+{
+    printf("december, 31 days");
+}
+return 0;
+}
