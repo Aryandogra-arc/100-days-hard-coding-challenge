3ba8340470e6be840480c59d663dbcf24db652cb
Aryandogra-arc <Aryan.40678@stu.upes.ac.in>
2026-09-01 23:25:33 +0530

    Add files via upload

---
+++ day21/ques2.c
@@ -0,0 +1,43 @@
+Q2: Write a program to check if a number is a perfect number.
+
+/*
+Sample Test Cases:
+Input 1:
+6
+Output 1:
+Perfect number
+
+Input 2:
+10
+Output 2:
+Not perfect number
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int num, i, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    for (i = 1; i < num; i++)
+    {
+        if (num % i == 0)
+        {
+            sum = sum + i;
+        }
+    }
+
+    if (sum == num)
+    {
+        printf("%d is a Perfect Number.\n", num);
+    }
+    else
+    {
+        printf("%d is Not a Perfect Number.\n", num);
+    }
+
+    return 0;
+}
