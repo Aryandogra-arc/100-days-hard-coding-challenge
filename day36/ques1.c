430a7e35c4e11260bc2a6872cbe5b7362ba9e0df
Aryandogra-arc <Aryan.40678@stu.upes.ac.in>
2026-09-14 22:11:42 +0530

    Add files via upload

---
+++ day36/ques1.c
@@ -0,0 +1,43 @@
+Q71: Read and print a matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+2 2
+1 2
+3 4
+Output 1:
+1 2
+3 4
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10], rows, cols, i, j;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < rows; i++)
+    {
+        for(j = 0; j < cols; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Matrix is:\n");
+    for(i = 0; i < rows; i++)
+    {
+        for(j = 0; j < cols; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
