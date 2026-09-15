9b3eee814a47288900070135700a3e64293147b7
Aryandogra-arc <Aryan.40678@stu.upes.ac.in>
2026-09-16 00:37:56 +0530

    Add files via upload

---
+++ day38/ques2.c
@@ -0,0 +1,63 @@
+Q76: Check if a matrix is symmetric.
+
+/*
+Sample Test Cases:
+Input 1:
+2 2
+1 2
+2 1
+Output 1:
+True
+
+Input 2:
+2 2
+1 0
+2 1
+Output 2:
+False
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10];
+    int r, c, i, j, flag = 1;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    if(r != c)
+    {
+        printf("False");
+        return 0;
+    }
+
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            if(a[i][j] != a[j][i])
+            {
+                flag = 0;
+                break;
+            }
+        }
+    }
+
+    if(flag == 1)
+        printf("True");
+    else
+        printf("False");
+
+    return 0;
+}
